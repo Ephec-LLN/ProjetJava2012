902d835a9bfd6786c940b8f175a5722e3961f1b6
Laurent Mondry <laurentmondry@gmail.com>
2012-12-08 22:53:46 +0100

    J'ai ajouté le code du menu

---
+++ src/solitaire/Menu.java
@@ -0,0 +1,51 @@
+package solitaire;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+
+
+public class Menu extends JMenuBar {
+
+	private static final long serialVersionUID = 1L;
+	private static PlateauTech plateau;
+	private static final String Aide = "Règles du jeu\n" +
+			"-------------\n\n"+
+			"Les regles sont simples, vous disposez d'un plateau\n" +
+			"en forme de croix, la case centrale etant vide, le\n" +
+			"reste est rempli de pions. Pour éliminer un pion,\n" +
+			"il suffit de sauter au dessus avec un autre pion. Le\n" +
+			"but étant de n'avoir plus qu'un pion, au centre du\n" +
+			"plateau.\n\nBon Jeu !";
+
+
+	public Menu(PlateauTech plateau) {
+		super();
+		Menu.plateau = plateau;
+		JMenuItem partie = new JMenuItem("Nouvelle Partie");
+		partie.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Menu.plateau.recommencer();
+			}
+		});
+		this.add(partie);
+		JMenuItem aide = new JMenuItem("Aide");
+		aide.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JOptionPane.showMessageDialog(null,Aide,"Aide",JOptionPane.INFORMATION_MESSAGE);
+			}
+		});	
+		this.add(aide);
+		JMenuItem apd = new JMenuItem("A propos");
+		apd.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JOptionPane.showMessageDialog(null,"Decembre 2012\nAuteurs : Jonathan et Laurent","A propos",JOptionPane.INFORMATION_MESSAGE);
+			}
+		});	
+		this.add(apd);
+	}
+
+}
