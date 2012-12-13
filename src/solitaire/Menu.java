75082f5817ff511e7abb740c268e0f798ccf0421
Laurent Mondry <laurentmondry@gmail.com>
2012-12-13 20:00:29 +0100

    Code
    J'essaye de rajouter un sous menu pour une eventuelle personnalisation
    du plateau et des pions. Pour le plateau j'ai deja mes petites idées en
    tête, je fais ça cette nuit ^^

--- src/solitaire/Menu.java
+++ src/solitaire/Menu.java
@@ -3,6 +3,7 @@ package solitaire;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
@@ -30,7 +31,7 @@ public class Menu extends JMenuBar {
 			public void actionPerformed(ActionEvent e) {
 				int reply = JOptionPane.showConfirmDialog(null, "Recommencer?", "Nouvelle Partie",  JOptionPane.YES_NO_OPTION);
 				if (reply == JOptionPane.YES_OPTION) {
-					Menu.plateau.recommencer();	
+					Menu.plateau.recommencer();
 				}
 			}
 		});
@@ -49,6 +50,10 @@ public class Menu extends JMenuBar {
 			}
 		});	
 		this.add(apd);
+		/*JMenuItem perso = new JMenuItem("Personnalisation");
+		JMenu submenu = new JMenu("test");
+		perso.add(submenu);
+		this.add(perso);*/
 	}
 
 }
