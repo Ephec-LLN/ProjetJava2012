28e6e6bc21cec734a1ee3aa7d7d9e455099fe0af
Laurent Mondry <laurentmondry@gmail.com>
2012-12-18 14:19:12 +0100

    package-info

---
+++ src/solitaire/package-info.java
@@ -0,0 +1,13 @@
+
+/** Fournit les classes nécessaire pour créer notre jeu :
+ * 
+ * la classe Jeu contenant la méthode main
+ * la classe Fond définissant l'image de fond de notre fenetre
+ * la classe Menu permettant de générer notre menu
+ * la classe PlateauTech permettant de gérer le plateau
+ * la classe PlateauUI permettant de générer le plateau 
+ * 
+ * @version 1.0
+ * @author Jonathan et Laurent 
+ */ 
+package solitaire;
